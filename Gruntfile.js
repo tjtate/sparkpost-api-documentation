61cbef1d8177f392a6c05428a0c30dfafd3be565
Cole Furfaro-Strode <cfurfarostrode@Coles-MacBook-Pro.local>
2015-04-22 14:49:37 -0400

    grunt task to create apiary.html from apiary.apib

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
-var matchdep = require('matchdep');
+var matchdep = require('matchdep')
+    , fs = require('fs')
+    , request = require('request');
 
 module.exports = function(grunt) {
     // Dynamically load any preexisting grunt tasks/modules
@@ -8,9 +10,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             options: {
-                banner: 'FORMAT: X-1A' + grunt.util.linefeed + 
-                    'HOST: https://api.sparkpost.com/api/v1' + 
-                    grunt.util.linefeed + grunt.util.linefeed
+                banner: 'FORMAT: X-1A' + grunt.util.linefeed +
+                'HOST: https://api.sparkpost.com/api/v1' +
+                grunt.util.linefeed + grunt.util.linefeed
             },
             prod: {
                 src: [
@@ -46,7 +48,34 @@ module.exports = function(grunt) {
             }
         }
     });
-    
+
+    grunt.registerTask('generate-apiary-preview', 'Creates apiary.html preview', function() {
+        var done = this.async()
+            , options = {
+                url: 'https://api.apiary.io/blueprint/generate',
+                body: fs.readFileSync('./apiary.apib', 'utf-8'),
+                headers: {
+                    'Accept': 'text/html',
+                    'Content-Type': 'text/plain'
+                }
+            };
+        
+        request.post(options, function(err, response, body) {
+            if (err || response.statusCode !== 200) {
+                grunt.log.error('There was an error trying to generate the preview for ' + file, err, response.statusCode, response.body);
+                done(false);
+            } else {
+                fs.writeFile('./apiary.html', body, function(err) {
+                    if (err) {
+                        grunt.log.error('There was an error trying to write to apiary.html', err);
+                        return done(false);
+                    }
+                    done();
+                });
+            }
+        });
+    });
+
     // grunt testFiles - runs apiary-blueprint-validator on individual blueprint files
     grunt.registerTask('testFiles', 'Validates individual blueprint files', [
         'shell:test:introduction.md',
@@ -61,9 +90,15 @@ module.exports = function(grunt) {
         'shell:test:smtp_api.md'
     ]);
 
+    //grunt preview - creates a livereloaded preview of the docs, Apiary-style
+    grunt.registerTask('preview', 'View the apiary generated HTML files in the browser', [
+        'compile',
+        'generate-apiary-preview'
+    ]);
+
     // grunt test - runs apiary-blueprint-validator on combined apiary.apib file
     grunt.registerTask('test', [ 'shell:test' ]);
-    
+
     // grunt compile - concatenates all the individual blueprint files and validates it
     grunt.registerTask('compile', [ 'concat:prod', 'test' ]);
 
