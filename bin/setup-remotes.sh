eccb566a9be1e144102fc96bcbc9b44ca99baab9
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-06-02 12:54:40 -0400

    Adding remote setup script

---
+++ bin/setup-remotes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# upstream: to the public sp.com repo
+git remote add upstream git@github.com:SparkPost/sparkpost-api-documentation.git
+
+# sp-elite: to sp-elite DEV repo
+git remote add sp-elite git@github.com:SparkPost/sp-elite-api-documentation-DEV.git
+
+# on-prem: to on-prem DEV repo
+git remote add on-prem git@github.com:SparkPost/on-prem-api-documentation-DEV.git
