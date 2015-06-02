78ea7357b9f64302ef147bc02315b594bf6d5b65
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-06-02 13:02:02 -0400

    Remote feedback added

--- bin/setup-remotes.sh
+++ bin/setup-remotes.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 # upstream: to the public sp.com repo
-git remote add upstream git@github.com:SparkPost/sparkpost-api-documentation.git
+git remote add upstream git@github.com:SparkPost/sparkpost-api-documentation.git > /dev/null 2>&1
 
 # sp-elite: to sp-elite DEV repo
-git remote add sp-elite git@github.com:SparkPost/sp-elite-api-documentation-DEV.git
+git remote add sp-elite git@github.com:SparkPost/sp-elite-api-documentation-DEV.git > /dev/null 2>&1
 
 # on-prem: to on-prem DEV repo
-git remote add on-prem git@github.com:SparkPost/on-prem-api-documentation-DEV.git
+git remote add on-prem git@github.com:SparkPost/on-prem-api-documentation-DEV.git > /dev/null 2>&1
+
+# print remotes
+echo "Remotes set up!"
+echo ""
+git remote -v
